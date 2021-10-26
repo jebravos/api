0d524374145af7ba37ce845240d2389d0b8ef993
jebravos <tc.jebravos@gmail.com>
2021-10-26 15:26:35 +0200

    Update ControllerApiApplication.java
    package updated

--- src/main/java/org/bravo/api/ControllerApiApplication.java
+++ src/main/java/org/bravo/api/ControllerApiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@EntityScan("org.iqvia.api.entity")
+@EntityScan("org.bravo.api.entity")
 @SpringBootApplication
 public class ControllerApiApplication {
 
